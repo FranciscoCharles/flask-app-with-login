df48d788570f7adcefcfb76aab4bb19bc55a8860
FranciscoCharles <charlesdcf123@gmail.com>
2021-10-14 10:02:14 -0300

    add function to create db if not exists

--- app_with_login/ext/command.py
+++ app_with_login/ext/command.py
@@ -1,29 +1,39 @@
-import toml
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import create_engine
-from app_with_login.models import User
 from app_with_login.ext.database import db
+from app_with_login.models import User
+import toml
 
+def create_db_if_not_exists():
+    all_settings = toml.load('settings.toml')
+    default_settings = all_settings['default']
 
-all_settings = toml.load('settings.toml')
-default_settings = all_settings['default']
+    DB_USER = default_settings['DB_USER']
+    DB_PASSWORD = default_settings['DB_PASSWORD']
+    DB_URL = default_settings['DB_URL']
+    DB_NAME = default_settings['DB_NAME']
+    DB_CONNECTOR = default_settings['DB_CONNECTOR']
 
-DB_USER = default_settings['DB_USER']
-DB_PASSWORD = default_settings['DB_PASSWORD']
-DB_URL = default_settings['DB_URL']
-DB_NAME = default_settings['DB_NAME']
-DB_CONNECTOR = default_settings['DB_CONNECTOR']
-
-DB_URI = f"{DB_CONNECTOR}://{DB_USER}:{DB_PASSWORD}@{DB_URL}/{DB_NAME}"
-engine = create_engine(DB_URI, echo=True)
-
+    full_db_url = f"{DB_CONNECTOR}://{DB_USER}:{DB_PASSWORD}@{DB_URL}"
+    engine = create_engine(full_db_url, echo=True)
+    engine.execute(f'CREATE DATABASE IF NOT EXISTS {DB_NAME}')
+    engine.dispose()
+    
 def create_db():
     """Creates database"""
-    db.create_all()
+    try:
+        create_db_if_not_exists()
+        db.create_all()
+    except SQLAlchemyError as error:
+        print('Create DB error:', error)
     #User.__table__.create(engine)
 
 def drop_db():
     """Drop database"""
-    db.drop_all()
+    try:
+        db.drop_all()
+    except SQLAlchemyError as error:
+        print('Drop DB error:', error)
     #User.__table__.drop(engine)
 
 def populate_db():
@@ -34,7 +44,6 @@ def populate_db():
     ]
     db.session.bulk_save_objects(data)
     db.session.commit()
-    return User.query.all()
 
 def init_app(app):
     for command in [create_db, drop_db, populate_db]:
